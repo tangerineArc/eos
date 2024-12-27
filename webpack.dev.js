f547a26e073937ce8145e076540ede0b92b2fa7b
Swagatam <swagatamheroz@outlook.com>
2024-12-27 21:08:59 +0530

    feat: add linter and formatter

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,9 +2,9 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    mode: "development",
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/template.html"]
-    }
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
 });
